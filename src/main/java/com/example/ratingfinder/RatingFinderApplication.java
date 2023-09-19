5b036339f1577c7bd5e18d291146aefe96b783d2
wwwcyh <69996173+wwwcyh@users.noreply.github.com>
2023-09-19 02:07:38 -0700

    users crud functions

--- src/main/java/com/example/ratingfinder/RatingFinderApplication.java
+++ src/main/java/com/example/ratingfinder/RatingFinderApplication.java
@@ -6,14 +6,24 @@ import com.example.ratingfinder.service.TrustedReviewScrape;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class RatingFinderApplication {
+@ComponentScan({"com.example.ratingfinder.controller","com.example.ratingfinder.config","com.example.ratingfinder.Model.ScrapeModel","com.example.ratingfinder.models","com.example.ratingfinder.Repository","com.example.ratingfinder.service"})
+
+public class RatingFinderApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RatingFinderApplication.class, args);
 
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 
 }
---
+++ src/main/java/com/example/ratingfinder/service/UserService.java
@@ -0,0 +1,50 @@
+package com.example.ratingfinder.service;
+
+import com.example.ratingfinder.Repository.UserRepository;
+import com.example.ratingfinder.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserRepository getUserRepository()
+    {
+        return userRepository;
+    }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(int userId) {
+        return userRepository.findById(userId);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(int userId) {
+        userRepository.deleteById(userId);
+    }
+    public User updateUser(int userId, String newUsername) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setUsername(newUsername);
+            return userRepository.save(user);
+        } else {
+            throw new IllegalArgumentException("User not found with user_id: " + userId);
+        }
+    }
+}
