e8b03c6d6358d8aa047108969dbdd420676ac90c
wwwcyh <69996173+wwwcyh@users.noreply.github.com>
2023-10-23 02:01:35 -0700

    signIN authentication and test api method. testing result: works

--- src/main/java/com/example/ratingfinder/RatingFinderApplication.java
+++ src/main/java/com/example/ratingfinder/RatingFinderApplication.java
@@ -1,12 +1,17 @@
 package com.example.ratingfinder;
 
 
-import com.example.ratingfinder.config.YoutubeConfigProperties;
-import com.example.ratingfinder.service.TrustedReviewScrape;
+//import com.example.ratingfinder.config.YoutubeConfigProperties;
+//import com.example.ratingfinder.service.TrustedReviewScrape;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class RatingFinderApplication {
